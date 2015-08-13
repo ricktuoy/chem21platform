d1f358bf95a19e8d33c017b55f84f8770c329169
Rick Taylor <rick.taylor@york.ac.uk>
2015-08-13 16:47:58 +0100

    Added testing env

--- chem21repo/settings/__init__.py
+++ chem21repo/settings/__init__.py
@@ -1,5 +1,7 @@
 import os
 if 'DJANGO_DEVELOPMENT' in os.environ:
 	from local import *
+elif 'DJANGO_TESTING' in os.environ:
+	from testing import *
 else:
 	from production import *
---
+++ chem21repo/settings/testing.py
@@ -0,0 +1,42 @@
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+from common import *
+import dj_database_url
+
+DEBUG=True
+TEMPLATE_DEBUG=True
+
+DATABASES = {
+    "default": dj_database_url.config(default='postgres://localhost'),
+}
+
+# Cache settings.
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
+    # Long cache timeout for staticfiles, since this is used heavily by the optimizing storage.
+    "staticfiles": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "TIMEOUT": 60 * 60 * 24 * 365,
+        "LOCATION": "staticfiles",
+    },
+}
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+        }
+    }
+}   
