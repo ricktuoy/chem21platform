f0bf4b6dfc2b9377ac20fa166d6e9fa806bc73c2
Rick Taylor <rick@rickdev>
2016-01-28 12:14:55 +0000

    Added mod_time field

---
+++ chem21repo/repo/migrations/0002_textversion_modified_time.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('repo', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='textversion',
+            name='modified_time',
+            field=models.DateTimeField(null=True, blank=True),
+        ),
+    ]
