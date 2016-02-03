58f27c173b8822d12edd5ee690076de25d07c0ab
Rick Taylor <rick@rickdev>
2016-02-03 14:57:21 +0000

    Push order

---
+++ chem21repo/repo/management/commands/push_orders.py
@@ -0,0 +1,33 @@
+
+from chem21repo.repo.models import Lesson
+from chem21repo.repo.models import Module
+from chem21repo.repo.models import Question
+from chem21repo.drupal import DrupalQuestion, DrupalLesson, DrupalCourse
+from django.core.management.base import BaseCommand
+from chem21repo.api_clients import C21RESTRequests
+
+
+class Command(BaseCommand):
+    help = 'Print nodes with text changes'
+
+    def handle(self, *args, **options):
+        api = C21RESTRequests()
+
+        print "*** LESSONS"
+        for lesson in Lesson.objects.all():
+            try:
+                node = DrupalLesson(id=lesson.remote_id, question_orders=lesson.child_orders)
+            except:
+                print "No id"
+                continue
+            api.push(node)
+        print "*** MODULES"
+        for module in Module.objects.all():
+            print module.name
+            try:
+                node = DrupalCourse(
+                    id=module.remote_id, lesson_orders=module.child_orders)
+            except:
+                print "No id"
+                continue
+            api.push(node)
