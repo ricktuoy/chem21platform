69226f09832b2bde165eb1022ce1d079635fa4ba
Rick Taylor <rick@rickdev>
2016-05-23 09:37:14 +0100

    Updated JS versioning

--- chem21repo/static/js/common.js
+++ chem21repo/static/js/common.js
@@ -4,7 +4,7 @@
 // except 'app' ones, 
 requirejs.config({
     "baseUrl": "/static/js/lib",
-    "urlArgs": "bust=008",
+    "urlArgs": "bust=009",
     "shim": {
         'jquery.colorbox': ['jquery'],
         'jquery.throttle-debounce': ['jquery'],
