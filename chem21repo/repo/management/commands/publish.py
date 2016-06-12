01db7b0e33fb9a4dc95afd7faa49654c0addab27
Rick Taylor <rick@ricktaylordesign.co.uk>
2016-06-12 16:02:19 +0100

    Added cookies, added legal to publish command

--- chem21repo/repo/management/commands/publish.py
+++ chem21repo/repo/management/commands/publish.py
@@ -30,9 +30,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if options['front']:
-            paths = frozenset(['/', '/about/'])
+            paths = frozenset(['/', '/about/', '/legal/'])
         else:
-            paths = frozenset(['/about/'])
+            paths = frozenset(['/about/', '/legal/'])
         if options['id']:
             ct = ContentType.objects.get(app_label="repo", model=options['type'])
             obj = ct.get_object_for_this_type(pk=options['id'])
