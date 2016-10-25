14ea6de7f0a436a4abffbb30cfdf5f9a00da810f
Rick Taylor <rick@rickdev>
2016-10-25 14:10:33 +0100

    Fix to glossary replacement (prevents glossary inside glossary)

--- chem21repo/repo/templatetags/glossary.py
+++ chem21repo/repo/templatetags/glossary.py
@@ -7,13 +7,33 @@ register = template.Library()
 
 @register.filter
 def glossarize(txt):
-
+    repls = []
     terms = {}
+    repl_terms = set([])
+    
+    def sub_fn(match):
+        term = match.group(2).lower()
+        if term not in repl_terms:
+            repl_terms.add(term)
+            desc_markup = "<a class=\"glossary_term\" href=\"#\" title=\"%s\">%s</a>" % (terms[term], match.group(2))
+            repls.append(desc_markup)
+            return "%s%s%s" % (match.group(1), desc_markup, match.group(3))
+        else:
+            return match.group(0)
+    
+    re_terms = []
     for term in GlossaryTerm.objects.all():
-        txt = re.sub("(\<p\>.*?)("+re.escape(term.name)+")(.*\<\/p\>)", "\g<1>[*[\g<2>]*]\g<3>", txt, count=1, flags=re.IGNORECASE)
-        terms[term.name] = term.description
+        terms[term.name.lower()] = term.description
+        re_terms.append(re.escape(term.name))
 
+    search_term = "|".join(re_terms)
+    logging.debug(search_term)
+    txt = re.sub("(\<p\>.*?)(%s)(.*\<\/p\>)" % search_term, sub_fn, txt, flags=re.IGNORECASE)
+        
+    """
     for term, desc in terms.iteritems():
         desc_markup = "<a class=\"glossary_term\" href=\"#\" title=\"%s\">\g<1></a>" % (desc)
         txt = re.sub("\[\*\[("+re.escape(term)+")\]\*\]", desc_markup, txt, count=1, flags=re.IGNORECASE)
+    return txt
+    """
     return txt
