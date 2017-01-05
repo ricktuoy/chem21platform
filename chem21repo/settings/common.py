8378ca6f295954960fd51768e7ab394c66fe58bf
Rick Taylor <rick@rickdev>
2017-01-05 09:52:39 +0000

    New log-in

--- chem21repo/settings/common.py
+++ chem21repo/settings/common.py
@@ -164,6 +164,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_EMAILS = [
     'katie.privett@york.ac.uk',
     'louise.summerton@york.ac.uk',
     'tom.dugmore@york.ac.uk',
-    'james.sherwood@york.ac.uk',]
+    'james.sherwood@york.ac.uk',
+    'jobie.kirkwood@york.ac.uk']
 
 WEB_ROOT = ''
