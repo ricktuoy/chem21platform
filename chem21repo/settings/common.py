712403ad2ed9f5259ae8eea647f8ea29f2ec6147
Rick Taylor <rick.taylor@york.ac.uk>
2020-04-20 16:50:24 +0100

    Settings: fix S3 AWS key env var name

--- chem21repo/settings/common.py
+++ chem21repo/settings/common.py
@@ -93,7 +93,7 @@ USE_TZ = False
 
 # AWS setup
 
-AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID", "")
+AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
 AWS_S3_REGION_NAME = os.environ.get("AWS_STORAGE_REGION", None)
