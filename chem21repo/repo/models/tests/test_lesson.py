9c1d4a7356569f00812813b95cb2563cad2a8e14
Rick Taylor <rick.taylor@york.ac.uk>
2020-04-28 09:08:17 +0100

    Data: remove orphaned/unused pages

--- chem21repo/repo/models/tests/test_lesson.py
+++ chem21repo/repo/models/tests/test_lesson.py
@@ -11,3 +11,17 @@ class LessonTestCase(TestCase):
         for lesson in all_lessons:
             parents = lesson.modules.all()
             self.assertFalse(len(parents) > 1)
+
+    def test_that_all_lesson_objects_with_question_attr_have_page_attr(self):
+        pss = True
+        all_lessons = Lesson.objects.all().exclude(archived=True)
+        for lesson in all_lessons:
+            if lesson.is_question:
+                if not lesson.page:
+                    print [lesson.title, lesson.pk, lesson.first_question.title, lesson.first_question.pk]
+                    try:
+                        print lesson.get_absolute_url()
+                    except AttributeError:
+                        print "Orphan"
+                    pss = False
+        self.assertTrue(pss)
